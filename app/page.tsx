cc04ea19e7b502f263d7575c16dc117199697e18
Simon Manzler <simanzler@gmail.com>
2026-02-17 12:08:03 -0500

    updated styling

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,13 @@ import Link from "next/link";
 
 export default async function Page() {
   return (
-    <div>
-      <h1>Practice</h1>
-      <Button asChild>
-        <Link href="/practice">Start New Test</Link>
-      </Button>
+    <div className="fixed inset-x-0 top-[80px] h-[calc(100dvh-80px)] flex justify-center items-center">
+      <div className="flex flex-col items-center gap-4">
+        <h1 className="text-4xl font-bold">US Civics Practice (2025)</h1>
+        <Button asChild>
+          <Link href="/practice">Practice</Link>
+        </Button>
+      </div>
     </div>
   );
 }
