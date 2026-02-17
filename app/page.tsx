2876e8f7a2efdce66247e941a51c2f0d7db0b4cf
Simon Manzler <simanzler@gmail.com>
2026-02-17 12:17:02 -0500

    updated size

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,9 @@ export default async function Page() {
   return (
     <div className="fixed inset-x-0 top-[80px] h-[calc(100dvh-80px)] flex justify-center items-center">
       <div className="flex flex-col items-center gap-4">
-        <h1 className="text-4xl font-bold">US Civics Practice (2025)</h1>
+        <h1 className="text-xl md:text-4xl font-bold">
+          US Civics Practice (2025)
+        </h1>
         <Button asChild>
           <Link href="/practice">Practice</Link>
         </Button>
